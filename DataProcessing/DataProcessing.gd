c8ac9cfa7c538b282dec4971772fbafe6671b516
ryaghain <139398364+ryaghain@users.noreply.github.com>
2025-12-31 10:26:25 -0500

    finished compiling the data for FilteredData

--- DataProcessing/DataProcessing.gd
+++ DataProcessing/DataProcessing.gd
@@ -2,15 +2,21 @@
 extends Node
 
 @export var word_list: JSON
-@export_file_path("*.txt") var raw_data_file_path
+@export var raw_data_folder_path: String = ""
 @export_tool_button("Process Raw Data") var process_raw_data = _process_raw_data
 @export_tool_button("Convert String To Dict") var convert_string_to_dict = _convert_string_to_dict
 @export_tool_button("Filter Data") var filter_data = _filter_data
 @export_tool_button("Run") var run = _run
 
-var raw_data_string: String
-var data: Dictionary[String, int] = {}
+var raw_data_strings: Array[String] = []
+var unfiltered_data: Dictionary[String, Array] = {}
 var filtered_data: Dictionary[String, int] = {}
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		get_viewport().visible = false
+		_run()
+		get_tree().quit()
 
 func _run() -> void:
 	_process_raw_data()
@@ -18,23 +24,49 @@ func _run() -> void:
 	_filter_data()
 
 func _process_raw_data() -> void:
-	raw_data_string = FileAccess.get_file_as_string(raw_data_file_path)
+	for file_path: String in DirAccess.get_files_at(raw_data_folder_path):
+		var data_string: String = FileAccess.get_file_as_string(raw_data_folder_path + "/" + file_path)
+		print("File '%s' string length: %s" % [file_path, data_string.length()])
+		raw_data_strings.append(data_string)
+
+	print("\nSource file count: %s\n" % raw_data_strings.size())
 
 func _convert_string_to_dict() -> void:
-	#var slice_count: int = raw_data_string.get_slice_count("\n")
-	#print("slice count: %s" % slice_count)
-	for substring: String in raw_data_string.split("\n"):
-		var substring_data: PackedStringArray = substring.split("\t")
-		data[substring_data[1]] = int(substring_data[2])
+	for file_string: String in raw_data_strings:
+		var file_lines: PackedStringArray = file_string.split("\n")
+		print("File line count: %s" % file_lines.size())
+
+		for file_line: String in file_lines:
+			var string_data: PackedStringArray = file_line.split("\t")
+
+			if string_data.size() == 3:
+				var word: String = string_data[1]
+
+				if unfiltered_data.has(word):
+					unfiltered_data[word].append(int(string_data[2]))
+				else:
+					unfiltered_data[word] = [int(string_data[2])]
+
+	print("\nUnfiltered data size: %s\n" % unfiltered_data.size())
 
 func _filter_data() -> void:
-	for word: String in data.keys():
+	for word: String in unfiltered_data.keys():
 		if word_list.data.has(word):
-			filtered_data[word] = data[word]
+			var average_frequency: int = 0
+			var frequencies: Array = unfiltered_data[word]
 
-	print(filtered_data.size())
+			for n: int in frequencies:
+				average_frequency += n
+			average_frequency = roundi(float(average_frequency) / float(frequencies.size()))
+
+			filtered_data[word] = average_frequency
+			print("%s: %s" % [word, average_frequency])
+
+	print("\nFiltered data size: %s\n" % filtered_data.size())
 
 	var file: FileAccess = FileAccess.open("res://Assets/FilteredData.json", FileAccess.WRITE)
 	file.store_line(JSON.stringify(filtered_data, "\t"))
 	file.flush()
 	file.close()
+
+	print("Saved data to 'res://Assets/FilteredData.json'.")
